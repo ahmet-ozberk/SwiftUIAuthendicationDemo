a8eacd541b17a33baeae585e7fcac6bf5886f968
Ahmet Özberk <ahmet.ozberk.mrs@gmail.com>
2022-04-08 22:29:53 +0300

    Initial Commit

---
+++ DemoAuthendication/DemoAuthendicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAuthendicationApp.swift
+//  DemoAuthendication
+//
+//  Created by Ahmet OZBERK on 8.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoAuthendicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
